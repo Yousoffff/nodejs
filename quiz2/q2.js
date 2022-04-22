2fa63d27a7fce1a1ff8468508e95cc1957eef316
saba bababeyk <90498006+bababeyksaba@users.noreply.github.com>
2022-04-22 21:55:17 +0430

    commit3

--- quiz2/q2.js
+++ quiz2/q2.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.get("/Math/:n1/:n2", (req, res) => {
+app.get("/Math", (req, res) => {
   let { n1, n2 } = req.query;
   const result=n1*n2;
 
   res.send(`<h2>Numbers is : ${n1} and ${n2}, Result is = ${result}</h2>`);
 });
 
-app.listen("4577")
+app.listen(4577)
